86f052bb47a9fb61d53f222a461cc3f9dd6c326e
Diana Deluvian <diana.deluvian@gmail.com>
2021-11-21 20:16:24 +0100

    recipe routes

---
+++ router/recipes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const connection = require('../Models/index');
+const Recipe = require('../Models/Recipe');
+
+router.get('/recipes', (req, res) => {
+    Recipe.find({}, (err, recipes) =>{
+    if(err) return handleError(err)
+    res.send(recipes)   
+    });
+});
+
+router.post('/recipe', (req, res) => {
+    console.log(req.body)
+    Recipe.create(req.body, function (err, recipe) {
+        if (err) return handleError(err);
+        console.log(recipe)
+        res.send(recipe);
+      });
+  });
+  
+router.put('/recipe/:id', (req, res) => {
+    Recipe.findOneAndUpdate(
+      {_id: req.params.id},
+      req.body,
+      {returnDocument: 'after'})
+    .then(result => res.send(result))
+    .catch(err => handleError(err))
+  });
+  
+router.delete('/recipe/:id', (req, res) => {
+    Recipe.deleteOne({_id: req.params.id})
+    .then(result => res.send(result))
+    .catch(err => handleError(err))
+  });
+
+module.exports = router
