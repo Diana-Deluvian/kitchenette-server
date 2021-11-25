67a5b50a1f90068ff8b9d3708a3825ebf4dd5ccf
Diana Deluvian <diana.deluvian@gmail.com>
2021-11-25 14:32:22 +0100

    added user schema

--- Models/index.js
+++ Models/index.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL
-, {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true
 });
 
