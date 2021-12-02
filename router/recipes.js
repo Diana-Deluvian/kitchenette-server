e0af6546140031c16d7c3eb2098d3e3368707f26
Diana Deluvian <diana.deluvian@gmail.com>
2021-12-02 12:36:55 +0100

    added image CDN to creating recipes

--- router/recipes.js
+++ router/recipes.js
@@ -2,7 +2,32 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const connection = require('../Models/index');
+let multer  = require('multer');
 const Recipe = require('../Models/Recipe');
+var cloudinary = require('cloudinary').v2;
+const { Readable } = require("stream");
+require('dotenv').config();
+
+const bufferToStream = (buffer) => {
+  const readable = new Readable({
+    read() {
+      this.push(buffer);
+      this.push(null);
+    },
+  });
+  return readable;
+}
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_KEY, 
+  api_secret: process.env.CLOUDINARY_SECRET,
+  secure: true
+});
+
+let upload  = multer({ dest: 'uploads/',storage: multer.memoryStorage() });
+
 
 router.get('/recipes', (req, res) => {
     Recipe.find({}, (err, recipes) =>{
@@ -11,14 +36,21 @@ router.get('/recipes', (req, res) => {
     });
 });
 
-router.post('/recipe', passport.authenticate('jwt', { session: false }), (req, res) => {
-    console.log(req.body)
-    Recipe.create(req.body, function (err, recipe) {
+router.post('/recipe', passport.authenticate('jwt', { session: false }), upload.single('somefile'), (req, res) => {
+  const stream = cloudinary.uploader.upload_stream(
+    { folder: "recipes" },
+    (error, result) => {
+      if (error) return console.error(error);
+      req.body.imgUrl = result.secure_url;
+      Recipe.create(req.body, function (err, recipe) {
         if (err) return handleError(err);
         console.log(recipe)
         res.send(recipe);
       });
-  });
+    }
+  );
+  bufferToStream(req.file.buffer).pipe(stream);
+});
   
 router.put('/recipe/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
     Recipe.findOneAndUpdate(
